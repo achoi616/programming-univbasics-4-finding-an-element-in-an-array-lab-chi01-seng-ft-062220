3b277fb4653c1f76d01762e3f86781edbc5c1589
achoi616 <achoi616@gmail.com>
2020-06-17 20:15:37 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  x = value_to_find
-  array = ["1", "2", "3", "0"]
-  new_array = array.index(1)
-end
+  if x = value_to_find 
+    array = ["1", "2", "3", "0"]
+    new_array = array.index(x)
+  elsif 
+    puts nil
+  end
+
+  
